209bae37dcc0a8e26499f42ade1a7471e0bab84a
JoeruCodes <joymedi8143@gmail.com>
2025-05-25 18:06:18 +0530

    pr url

--- ssr/src/page/src/wallet/mod.rs
+++ ssr/src/page/src/wallet/mod.rs
@@ -295,7 +295,7 @@ pub fn NotificationWalletImpl() -> impl IntoView {
     });
 
     let on_token_click: Action<(), (), LocalStorage> = Action::new_unsync(move |()| async move {
-        let metaclient: MetadataClient<false> = MetadataClient::default();
+        let metaclient: MetadataClient<false> = MetadataClient::with_base_url(reqwest::Url::parse("https://pr-31-dolr-ai-yral-metadata.fly.dev/").unwrap());
 
         let cans = Canisters::from_wire(auth_cans.await.unwrap(), expect_context()).unwrap();
 
