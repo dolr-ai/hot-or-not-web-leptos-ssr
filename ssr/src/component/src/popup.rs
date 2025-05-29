28b56ce960832b4e4c563e948506bbfd2e9f6084
harsh-mn-yral <harsh@gobazzinga.io>
2025-05-29 16:14:38 +0530

    fix

--- ssr/src/component/src/popup.rs
+++ ssr/src/component/src/popup.rs
@@ -6,11 +6,11 @@ use leptos::prelude::*;
 pub fn Popup(#[prop(into)] show: RwSignal<bool>, children: ChildrenFn) -> impl IntoView {
     view! {
         <ShadowOverlay show>
-            <div class="mx-4 py-4 px-[20px] max-w-full relative max-h-full items-center gap-5 cursor-auto flex-col flex justify-between bg-neutral-900 rounded-md">
+            <div style="min-height: 500px" class="mx-4 py-4 px-[20px] max-w-full relative max-h-full items-center gap-5 cursor-auto flex-col flex justify-between bg-neutral-900 rounded-md">
                 <div class="pb-4 w-full flex-1">{children()}</div>
-                <div class="flex justify-center w-full items-center">
+                <div class="flex justify-center w-full items-center px-8">
                     <HighlightedButton
-                        alt_style=false
+                        alt_style=true
                         disabled=false
                         on_click=move || show.set(false)
                         classes="w-full".to_string()
--- ssr/src/page/src/token/icpump_sunset_popup.rs
+++ ssr/src/page/src/token/icpump_sunset_popup.rs
@@ -9,12 +9,12 @@ pub fn IcpumpSunsetPopup() -> impl IntoView {
     view! {
         <Popup show={icpump_sunset_popup_ctx.show}>
             <div class="flex flex-col gap-4 text-center">
-                <h1 class="text-4xl font-bold">"👋"</h1>
-                <div class="text-xl">
-                "icpump.fun is being sunset soon."
+                <h1 style="font-size: 3.5rem" class="font-bold pt-24">"👋"</h1>
+                <div class="text-3xl font-semibold text-neutral-100">
+                    "ICPum.fun is being sunset soon."
                 </div>
-                <div class="text-neutral-500">
-                "All created tokens will be dissolved. In case of any concerns, please reach out to us on " <a href="https://t.me/HotOrNot_app" target="_blank" class="underline">Telegram</a>
+                <div class="text-neutral-200 text-xl">
+                    "All created tokens will be dissolved. In case of any concerns, <br/>please reach out to us on " <a href="https://t.me/HotOrNot_app" target="_blank" class="underline">Telegram</a>
                 </div>
             </div>
         </Popup>
