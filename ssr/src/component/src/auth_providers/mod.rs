80fb47e17cd6104f4d8a65e16dc425f50b209830
komal-sai-yral <komalsai@gobazzinga.io>
2025-06-02 17:09:50 +0530

    cleanup comments

--- ssr/src/component/src/auth_providers/mod.rs
+++ ssr/src/component/src/auth_providers/mod.rs
@@ -156,7 +156,7 @@ pub fn LoginProviders(
         // Capture the context signal setter
         send_wrap(async move {
             let referrer = auth.referrer_store.get_untracked();
-            leptos::logging::log!("LoginProviders referrer: {:?}", referrer);
+
             auth.set_new_identity(id.clone(), true);
 
             let canisters = Canisters::authenticate_with_network(id, referrer).await?;
