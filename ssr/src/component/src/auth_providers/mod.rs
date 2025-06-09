d87a2385a12f9cee601772e0a78ffa625378b8be
Harsha <106158714+harsh-mn-yral@users.noreply.github.com>
2025-06-09 09:18:38 +0000

    use backend

--- ssr/src/component/src/auth_providers/mod.rs
+++ ssr/src/component/src/auth_providers/mod.rs
@@ -2,7 +2,7 @@
 pub mod yral;
 use candid::Principal;
 use consts::NEW_USER_SIGNUP_REWARD;
-use consts::REFERRAL_REWARD;
+use hon_worker_common::limits::REFERRAL_REWARD;
 use hon_worker_common::sign_referral_request;
 use hon_worker_common::ReferralReqWithSignature;
 use ic_agent::Identity;
