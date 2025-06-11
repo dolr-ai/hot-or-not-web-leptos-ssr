594399b05a909a7aac160ef8c7ae319a0c0efb64
tushar-saxena-yral <tushar@gobazzinga.io>
2025-06-11 10:28:30 +0530

    chore: update the airdrop limits

--- ssr/src/consts/src/lib.rs
+++ ssr/src/consts/src/lib.rs
@@ -14,7 +14,7 @@ pub use remote::*;
 use once_cell::sync::Lazy;
 use reqwest::Url;
 
-pub const SATS_AIRDROP_LIMIT_RANGE: Range<u64> = 100..1000;
+pub const SATS_AIRDROP_LIMIT_RANGE: Range<u64> = 50..100;
 pub const CENTS_IN_E6S: u64 = 1_000_000;
 pub const CF_STREAM_BASE: &str = "https://customer-2p3jflss4r4hmpnz.cloudflarestream.com";
 pub const FALLBACK_PROPIC_BASE: &str = "https://api.dicebear.com/7.x/big-smile/svg";
