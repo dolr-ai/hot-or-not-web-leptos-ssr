c7bcc887a6033e0d2a701b9191ab983d6e2c7743
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2025-06-23 15:29:27 +0530

    Using use_coockie instead of use_coookie_with_option

--- ssr/src/component/src/nav.rs
+++ ssr/src/component/src/nav.rs
@@ -8,7 +8,7 @@ use consts::{
 use leptos::{either::Either, prelude::*};
 use leptos_icons::*;
 use leptos_router::hooks::use_location;
-use leptos_use::{storage::use_local_storage, use_cookie_with_options, UseCookieOptions};
+use leptos_use::{storage::use_local_storage, use_cookie, use_cookie_with_options, UseCookieOptions};
 use utils::{
     mixpanel::mixpanel_events::{
         BottomNavigationCategory, MixPanelEvent, MixpanelBottomNavigationProps, MixpanelGlobalProps,
@@ -208,18 +208,15 @@ fn NavIcon(
 
 #[component]
 fn UploadIcon(#[prop(into)] cur_selected: Signal<bool>) -> impl IntoView {
-    let (user_principal, _) = use_cookie_with_options::<Principal, FromToStringCodec>(
+    let (user_principal, _) = use_cookie::<Principal, FromToStringCodec>(
         USER_PRINCIPAL_STORE,
-        UseCookieOptions::default(),
     );
 
-    let (user_canister, _) = use_cookie_with_options::<Principal, FromToStringCodec>(
+    let (user_canister, _) = use_cookie::<Principal, FromToStringCodec>(
         USER_CANISTER_ID_STORE,
-        UseCookieOptions::default(),
     );
-    let (is_connected, _) = use_cookie_with_options::<bool, FromToStringCodec>(
+    let (is_connected, _) = use_cookie::<bool, FromToStringCodec>(
         ACCOUNT_CONNECTED_STORE,
-        UseCookieOptions::default(),
     );
     let (is_nsfw_enabled, _, _) = use_local_storage::<bool, FromToStringCodec>(NSFW_TOGGLE_STORE);
 
