73c66fd8e99c2cf1c52de0040df143cd0ee914da
Prakash <prakash@gobazzinga.io>
2025-06-26 07:48:42 +0000

    Removed extra openid from scopes

--- ssr/src/auth/src/server_impl/yral.rs
+++ ssr/src/auth/src/server_impl/yral.rs
@@ -118,7 +118,7 @@ pub async fn yral_auth_url_impl(
         Nonce::new_random,
     );
 
-    for scope in &["openid", "email"] {
+    for scope in &["email"] {
         oauth2_request = oauth2_request.add_scope(Scope::new(scope.to_string()));
     }
 
