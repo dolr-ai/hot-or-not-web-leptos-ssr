dcdacf667f0275b16f2b698e14787b91ef6edf91
JoeruCodes <joymedi8143@gmail.com>
2025-07-01 13:23:25 +0530

    chore: update dependencies and add new modules for notifications and testing
    * Downgraded `windows-sys` dependency to version 0.48.0.
    * Added new packages: `scoped-tls`, `spacetimedb`, `spacetimedb-bindings-sys`, and `yral-spacetime-utils`.
    * Introduced a new `Test` module and updated routing in the app.
    * Implemented a function to retrieve notifications in the airdrop state management.

---
+++ ssr/src/page/src/test.rs
@@ -0,0 +1,48 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn Test() -> impl IntoView {
+    use candid::Principal;
+    use codee::string::FromToStringCodec;
+    use consts::USER_PRINCIPAL_STORE;
+    use leptos_use::use_cookie;
+
+    #[cfg(feature = "stdb-backend")]
+    let ctx: state::stdb_dolr_airdrop::WrappedContext = expect_context();
+
+    #[cfg(feature = "stdb-backend")]
+    let notifications = RwSignal::new(None);
+
+    let (user_principal_cookie, _) =
+        use_cookie::<Principal, FromToStringCodec>(USER_PRINCIPAL_STORE);
+
+    Effect::new(move |_| {
+        #[cfg(feature = "stdb-backend")]
+        {
+            use state::stdb_dolr_airdrop::get_notitfication;
+
+            notifications
+                .set(get_notitfication(user_principal_cookie.get().unwrap(), &ctx.conn).ok())
+        }
+    });
+    view! {
+        <div>
+            <h1>Test</h1>
+            <div>
+                <h2>Notifications</h2>
+                <ul>
+
+                    {
+                        #[cfg(feature = "stdb-backend")]
+                        move || {
+                            let res = format!("{:?}", notifications.get().unwrap());
+                            view! {
+                                <li>{res}</li>
+                            }
+                        }
+                    }
+                </ul>
+            </div>
+        </div>
+    }
+}
