5a975701402f8b10d0ff623c07e9cd19e7da6f26
Harsha <106158714+harsh-mn-yral@users.noreply.github.com>
2025-07-24 06:40:17 +0000

    hide on web

--- ssr/src/page/src/post_view/overlay.rs
+++ ssr/src/page/src/post_view/overlay.rs
@@ -581,8 +581,9 @@ fn ExpandableText(description: String) -> impl IntoView {
 pub fn MuteUnmuteButton(muted: RwSignal<bool>) -> impl IntoView {
     view! {
         <button
-            class="absolute z-10 rounded-r-lg bg-black/25 py-2 px-3 cursor-pointer text-sm font-medium gap-1 text-white top-[7rem] flex items-center left-0
-            hover:translate-x-0 active:translate-x-0 -translate-x-2/3 focus:delay-2000 focus:hover:delay-0 focus:active:delay-0 transition-all"
+            class="z-10 rounded-r-lg bg-black/25 py-2 px-3 cursor-pointer text-sm font-medium gap-1 text-white top-[7rem] flex items-center left-0
+            [@media (pointer: coarse)]:absolute [@media (pointer:fine)]:hidden
+            active:translate-x-0 -translate-x-2/3 focus:delay-2000 focus:active:delay-0 transition-all"
             on:click=move |_| muted.set(!muted.get_untracked())
         >
             <div class="w-[8ch] text-center">{move || if muted.get() { "Unmute" } else { "Mute" }}</div>
