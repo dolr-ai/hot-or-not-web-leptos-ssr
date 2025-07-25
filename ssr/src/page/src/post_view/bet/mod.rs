5cbdb4a4a3dfa7d3742d2dd790e955a9506805e7
Prakash <prakash@gobazzinga.io>
2025-07-25 09:13:20 +0530

    On visit to played outcome game, show the correct balance from localstorage (#1020)
    * On visit to played outcome game, show the correct balance instead of zero by fetching the balance from the endpoint

--- ssr/src/page/src/post_view/bet/mod.rs
+++ ssr/src/page/src/post_view/bet/mod.rs
@@ -223,7 +223,7 @@ fn HNButtonOverlay(
 
                         HnBetState::set(post_mix.uid.clone(), res.video_comparison_result);
 
-                        set_wallet_balalnce_store.set(match res.game_result.game_result.clone() {
+                        let balance = match res.game_result.game_result.clone() {
                             GameResultV2::Win {
                                 win_amt: _,
                                 updated_balance,
@@ -232,7 +232,9 @@ fn HNButtonOverlay(
                                 lose_amt: _,
                                 updated_balance,
                             } => updated_balance.to_u64().unwrap_or(0),
-                        });
+                        };
+                        HnBetState::set_balance(balance);
+                        set_wallet_balalnce_store.set(balance);
 
                         MixPanelEvent::track_game_played(
                             global,
@@ -369,25 +371,37 @@ fn HNWonLost(
     let creator_reward_text = if creator_reward > 0 {
         format!(", creator gets {creator_reward} SATS")
     } else {
-        "".to_string()
+        String::new()
     };
     let (line1, line2) = match game_result.clone() {
-        GameResult::Win { win_amt } => (
-            format!("You voted \"{bet_direction_text}\" - Spot on!"),
-            format!(
-                "You won {} SATS{}",
-                TokenBalance::new((win_amt + vote_amount).into(), 0).humanize(),
-                creator_reward_text
-            ),
-        ),
-        GameResult::Loss { lose_amt } => (
-            format!("You voted \"{bet_direction_text}\" - wrong vote."),
-            format!(
-                "You lost {} SATS{}",
-                TokenBalance::new(lose_amt.into(), 0).humanize(),
-                creator_reward_text
-            ),
-        ),
+        GameResult::Win { win_amt } => {
+            let total_win = TokenBalance::new((win_amt + vote_amount).into(), 0).humanize();
+            if bet_direction_text.is_empty() {
+                (
+                    format!("You won {total_win} SATS",),
+                    "Tap ? to see how it works".into(),
+                )
+            } else {
+                (
+                    format!("You voted \"{bet_direction_text}\" - Spot on!"),
+                    format!("You won {total_win} SATS{creator_reward_text}",),
+                )
+            }
+        }
+        GameResult::Loss { lose_amt } => {
+            let total_loss = TokenBalance::new(lose_amt.into(), 0).humanize();
+            if bet_direction_text.is_empty() {
+                (
+                    format!("You lost {total_loss} SATS"),
+                    "Tap ? to see how it works".into(),
+                )
+            } else {
+                (
+                    format!("You voted \"{bet_direction_text}\" - wrong vote."),
+                    format!("You lost {total_loss} SATS{creator_reward_text}"),
+                )
+            }
+        }
     };
 
     let bet_amount = vote_amount;
@@ -420,14 +434,6 @@ fn HNWonLost(
             show_help_ping.set(false);
         }
     });
-
-    let (wallet_balance_store, _, _) =
-        use_local_storage::<u64, FromToStringCodec>(WALLET_BALANCE_STORE_KEY);
-
-    let total_balance_text = move || {
-        let balance = wallet_balance_store.get();
-        format!("Total balance: {balance} SATS")
-    };
 
     let show_ping = move || show_help_ping.get() && !won;
 
@@ -494,10 +500,34 @@ fn HNWonLost(
                         />
                     }})
             }
-            <div class=format!("flex items-center text-white text-sm font-semibold justify-center p-2 rounded-full {}", if won { "bg-[#158F5C]" } else { "bg-[#F14331]" })>
+            <TotalBalance won />
+        </div>
+    }
+}
+
+#[component]
+fn TotalBalance(won: bool) -> impl IntoView {
+    let (wallet_balance_store, _, _) =
+        use_local_storage::<u64, FromToStringCodec>(WALLET_BALANCE_STORE_KEY);
+
+    let total_balance_text = move || {
+        let balance = HnBetState::get_balance().unwrap_or(0);
+        format!("Total balance: {balance} SATS")
+    };
+
+    Effect::new(move |_| {
+        if HnBetState::get_balance().is_none() {
+            let balance = wallet_balance_store.get();
+            HnBetState::set_balance(balance);
+        }
+    });
+
+    view! {
+        <Show when=move|| HnBetState::get_balance().is_some()>
+            <div class=format!("flex items-center text-sm font-semibold justify-center p-2 rounded-full {}", if won { "bg-[#158F5C]" } else { "bg-[#F14331]" })>
                 {total_balance_text}
             </div>
-        </div>
+        </Show>
     }
 }
 
@@ -631,7 +661,6 @@ pub fn HNGameOverlay(
 
     view! {
         <Suspense fallback=LoaderWithShadowBg>
-
             {move || {
                 create_game_info
                     .get()
