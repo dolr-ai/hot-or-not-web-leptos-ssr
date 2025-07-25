3636b1d085364611e4ab5497218deedc2e8823cf
Harsha <106158714+harsh-mn-yral@users.noreply.github.com>
2025-07-25 12:03:27 +0000

    fix ios

--- ssr/src/page/src/post_view/overlay.rs
+++ ssr/src/page/src/post_view/overlay.rs
@@ -584,9 +584,10 @@ pub fn MuteUnmuteControl(muted: RwSignal<bool>, volume: RwSignal<f64>) -> impl I
     let volume_ = Signal::derive(move || if muted.get() { 0.0 } else { volume.get() });
     view! {
         <button
-            class="z-10 rounded-r-lg bg-black/25 py-2 px-3 cursor-pointer text-sm font-medium text-white items-center gap-1
+            tabindex="0"
+            class="z-10 select-none rounded-r-lg bg-black/25 py-2 px-3 cursor-pointer text-sm font-medium text-white items-center gap-1
             pointer-coarse:flex pointer-fine:hidden absolute top-[7rem] left-0
-            active:translate-x-0 -translate-x-2/3 focus:delay-[3.5s] focus:active:delay-0 transition-all"
+            active:translate-x-0 -translate-x-2/3 focus:delay-[3.5s] active:focus:delay-0 transition-all"
             on:click=move |_| {
                 let is_muted = muted.get_untracked();
                 muted.set(!is_muted);
@@ -601,7 +602,7 @@ pub fn MuteUnmuteControl(muted: RwSignal<bool>, volume: RwSignal<f64>) -> impl I
                 <SoundOffIcon classes="w-4 h-4".to_string() />
             </Show>
         </button>
-        <div class="z-10 rounded-full bg-black/35 p-2.5 cursor-pointer text-sm font-medium text-white items-center gap-3
+        <div class="z-10 select-none rounded-full bg-black/35 p-2.5 cursor-pointer text-sm font-medium text-white items-center gap-3
             pointer-coarse:hidden pointer-fine:flex absolute top-[7rem] left-4
             size-11 hover:size-auto group">
             <button
