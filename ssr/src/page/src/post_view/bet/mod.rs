a2fbdd4a19b45227baec523f5277557263182c5f
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2025-07-28 16:09:08 +0530

    Reloading the app after 5 sec

--- ssr/src/page/src/post_view/bet/mod.rs
+++ ssr/src/page/src/post_view/bet/mod.rs
@@ -272,10 +272,11 @@ fn HNButtonOverlay(
 
     let UseTimeoutFnReturn { start, .. } = use_timeout_fn(
         move |_: ()| {
-            // let url = format!(
-            //     "/hot-or-not/{}/{}",
-            //     login_post.canister_id, login_post.post_id
-            // );
+            let url = format!(
+                "/hot-or-not/{}/{}",
+                login_post.canister_id, login_post.post_id
+            );
+            let _ = window().location().set_href(&url);
             let _ = window().location().reload();
         },
         5000.0,
@@ -283,7 +284,7 @@ fn HNButtonOverlay(
 
     Effect::new(move |_| {
         let is_now = is_connected.get();
-        let was = was_connected.get();
+        let was = was_connected.get_untracked();
         if !was && is_now {
             was_connected.set(true);
             start(());
@@ -300,7 +301,7 @@ fn HNButtonOverlay(
             </button>
         </div>
         <LoginNudgePopup show=show_login_nudge show_login_popup  ev_ctx coin/>
-        <LoginModal show=show_login_popup redirect_to=Some(format!("/hot-or-not/{}/{}", login_post.canister_id, login_post.post_id)) />
+        <LoginModal show=show_login_popup redirect_to=None />
         <div class="flex flex-row gap-6 justify-center items-center w-full touch-manipulation">
             <HNButton disabled=running bet_direction kind=VoteKind::Hot place_bet_action />
             <button disabled=running on:click=move |_| coin.update(|c| *c = c.wrapping_next())>
