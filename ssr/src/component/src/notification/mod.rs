b7bc1678ae768a4e06d7fac2481d9442edddc2b0
JoeruCodes <joymedi8143@gmail.com>
2025-08-01 17:50:43 +0530

    Update NotificationPage styles for full-height layout consistency

--- ssr/src/component/src/notification/mod.rs
+++ ssr/src/component/src/notification/mod.rs
@@ -227,7 +227,7 @@ pub fn NotificationPage(close: RwSignal<bool>) -> impl IntoView {
             view! {
                 <ShadowOverlay show=ShowOverlay::Closable(close)>
                     <div class="relative w-screen min-h-screen h-full">
-                        <div class="flex flex-col items-center pt-4 pb-12 text-white bg-black">
+                        <div class="flex flex-col items-center pt-4 pb-12 text-white bg-black h-full">
                             <div class="sticky top-0 z-10 w-full bg-black">
                                 <TitleText justify_center=false>
                                     <div class="relative flex items-center justify-center">
