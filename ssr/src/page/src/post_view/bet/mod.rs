42c8140dd68ace5ce9ba593eefae36f32fe9cf60
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2025-08-06 17:52:20 +0530

    Removed show

--- ssr/src/page/src/post_view/bet/mod.rs
+++ ssr/src/page/src/post_view/bet/mod.rs
@@ -319,7 +319,7 @@ fn HNButtonOverlay(
             </button>
         </div>
         <LoginNudgePopup show=show_login_nudge show_login_popup  ev_ctx coin/>
-        <Show when=move||check_current_post()> <LoginModal show=show_login_popup redirect_to=Some(format!("/hot-or-not/{}/{}", login_post.canister_id, login_post.post_id)) /></Show>
+        <LoginModal show=show_login_popup redirect_to=None />
         <div class="flex flex-row gap-6 justify-center items-center w-full touch-manipulation">
             <HNButton disabled=running bet_direction kind=VoteKind::Hot place_bet_action />
             <button disabled=running on:click=move |_| coin.update(|c| *c = c.wrapping_next())>
