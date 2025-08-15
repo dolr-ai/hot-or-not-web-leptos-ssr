7b8ec6374945809df045b2d5ff405fa334437244
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2025-08-15 17:04:12 +0530

    Remove unused image element from HNWonLost component

--- ssr/src/page/src/post_view/bet/mod.rs
+++ ssr/src/page/src/post_view/bet/mod.rs
@@ -527,7 +527,6 @@ fn HNWonLost(
             <div class="flex gap-2 justify-center items-center w-full">
                 <div class="relative shrink-0 drop-shadow-lg">
                     <CoinStateView class="w-14 h-14 md:w-16 md:h-16" coin is_connected />
-                    // <img src=vote_kind_image class="absolute bottom-0 -right-1 h-7 w-7" />
                 </div>
                 <div class="flex-1 p-1 text-xs md:text-sm font-semibold leading-snug text-white rounded-full">
                     {line1}<br/>
