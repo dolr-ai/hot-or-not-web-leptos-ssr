0736aa6c7fe0b65a587ba1f24b5200d3fe02ad8e
tushar-saxena-yral <tushar@gobazzinga.io>
2025-08-28 12:39:15 +0530

    fix: lint check

--- ssr/src/page/src/wallet/airdrop/dolr_airdrop.rs
+++ ssr/src/page/src/wallet/airdrop/dolr_airdrop.rs
@@ -8,8 +8,8 @@ mod mock;
 
 #[server(endpoint = "dolr_airdrop_eligibility", input = server_fn::codec::Json)]
 pub async fn is_user_eligible_for_dolr_airdrop(
-    user_canister: Principal,
-    user_principal: Principal,
+    _user_canister: Principal,
+    _user_principal: Principal,
 ) -> Result<super::AirdropStatus, ServerFnError> {
     // #[cfg(not(feature = "dolr-airdrop"))]
     // use mock::is_user_eligible_for_dolr_airdrop as call;
@@ -23,8 +23,8 @@ pub async fn is_user_eligible_for_dolr_airdrop(
 
 #[server(endpoint = "claim_dolr_airdrop", input = server_fn::codec::Json)]
 pub async fn claim_dolr_airdrop(
-    user_canister: Principal,
-    user_principal: Principal,
+    _user_canister: Principal,
+    _user_principal: Principal,
 ) -> Result<u64, ServerFnError> {
     // #[cfg(not(feature = "dolr-airdrop"))]
     // use mock::claim_dolr_airdrop as call;
