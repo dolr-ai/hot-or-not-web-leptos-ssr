d4f6cc9014f35af4bb71e77153d6c2e683568594
Ravi Sawlani <152961362+ravi-sawlani-yral@users.noreply.github.com>
2025-09-03 14:57:15 +0000

    use new pagination query to fetch paginated posts from post service canister

--- ssr/src/utils/src/profile.rs
+++ ssr/src/utils/src/profile.rs
@@ -2,7 +2,9 @@ use candid::Principal;
 use ic_agent::AgentError;
 use indexmap::IndexSet;
 use leptos::prelude::*;
-use yral_canisters_client::{ic::USER_INFO_SERVICE_ID, individual_user_template::Result6};
+use yral_canisters_client::{
+    ic::USER_INFO_SERVICE_ID, individual_user_template::Result6, user_post_service::Result3,
+};
 
 use yral_canisters_common::{
     cursored_data::{CursoredDataProvider, PageEntry},
@@ -52,13 +54,26 @@ impl CursoredDataProvider for PostsProvider {
             USER_INFO_SERVICE_ID => {
                 let post_service_canister = self.canisters.user_post_service().await;
                 let limit = end - start;
-                let posts = post_service_canister
-                    .get_posts_of_this_user_profile_with_pagination_cursor(
+                let posts_res = post_service_canister
+                    .get_posts_of_this_user_profile_with_pagination(
                         self.user_principal,
                         start as u64,
                         limit as u64,
                     )
                     .await?;
+
+                let posts = match posts_res {
+                    Result3::Ok(posts) => posts,
+                    Result3::Err(get_posts_of_user_profile_error) => {
+                        log::warn!("failed to get posts {get_posts_of_user_profile_error:?}");
+                        return Ok(PageEntry {
+                            data: vec![],
+                            end: true,
+                        });
+                    }
+                };
+
+                let list_end = posts.len() < (end - start);
 
                 let post_details: Vec<PostDetails> = posts
                     .into_iter()
@@ -73,7 +88,7 @@ impl CursoredDataProvider for PostsProvider {
 
                 Ok(PageEntry {
                     data: post_details,
-                    end: true,
+                    end: list_end,
                 })
             }
             _ => {
