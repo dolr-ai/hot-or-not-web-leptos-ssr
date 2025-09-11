eb1ea006959265d7a15dd9d97dfb90289c1b99bb
tushar-saxena-yral <tushar@gobazzinga.io>
2025-09-11 17:56:11 +0530

    feat: move tracing behind feature flag

--- ssr/src/main.rs
+++ ssr/src/main.rs
@@ -110,6 +110,170 @@ async fn main_impl() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
 
     // Initialize telemetry if OTLP_ENDPOINT is configured
+    #[cfg(feature = "enable-oltp")]
+    let telemetry_handles = setup_telemetry();
+
+    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
+    // For deployment these variables are:
+    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
+    // Alternately a file can be specified such as Some("Cargo.toml")
+    // The file would need to be included with the executable when moved to deployment
+    let conf = get_configuration(None).unwrap();
+    let leptos_options = conf.leptos_options;
+    let addr = leptos_options.site_addr;
+    let routes = generate_route_list(App);
+
+    let res = AppStateBuilder::new(leptos_options, routes.clone())
+        .build()
+        .await;
+    let terminate = {
+        use tokio::signal;
+
+        let ctrl_c = async {
+            signal::ctrl_c()
+                .await
+                .expect("failed to install Ctrl+C handler");
+        };
+
+        #[cfg(unix)]
+        let terminate = async {
+            use tokio::signal;
+            signal::unix::signal(signal::unix::SignalKind::terminate())
+                .expect("failed to install signal handler")
+                .recv()
+                .await;
+        };
+
+        #[cfg(not(unix))]
+        let terminate = std::future::pending::<()>();
+
+        async {
+            tokio::select! {
+                _ = ctrl_c => {},
+                _ = terminate => {},
+            }
+            log::info!("stopping...");
+
+            #[cfg(feature = "local-bin")]
+            std::mem::drop(res.containers);
+        }
+    };
+
+    // Create HTTP tracing layer with OpenTelemetry semantic conventions
+    #[cfg(feature = "enable-oltp")]
+    let trace_layer = tower_http::trace::TraceLayer::new_for_http()
+        .make_span_with(|request: &axum::extract::Request<_>| {
+            let method = request.method();
+            let uri = request.uri();
+            let route = request
+                .extensions()
+                .get::<axum::extract::MatchedPath>()
+                .map(|path| path.as_str())
+                .unwrap_or_else(|| uri.path());
+
+            tracing::info_span!(
+                "http_request",
+                method = %method,
+                uri = %uri,
+                route = route,
+                // OpenTelemetry semantic conventions
+                otel.name = format!("{} {}", method, route),
+                otel.kind = "server",
+                http.method = %method,
+                http.url = %uri,
+                http.route = route,
+                service.name = "yral_ssr",
+            )
+        })
+        .on_response(
+            |response: &axum::response::Response,
+             latency: std::time::Duration,
+             _span: &tracing::Span| {
+                tracing::info!(
+                    status_code = response.status().as_u16(),
+                    latency_ms = latency.as_millis(),
+                    "request completed"
+                );
+            },
+        );
+
+    let sentry_tower_layer = ServiceBuilder::new()
+        .layer(NewSentryLayer::new_from_top())
+        .layer(SentryHttpLayer::with_transaction());
+
+    // build our application with a route
+    let app = Router::new()
+        .route(
+            "/api/{*fn_name}",
+            get(server_fn_handler).post(server_fn_handler),
+        )
+        .layer(
+            CorsLayer::new()
+                .allow_credentials(true)
+                .allow_headers([
+                    header::AUTHORIZATION,
+                    header::CONTENT_TYPE,
+                    header::ACCEPT,
+                    HeaderName::from_static("sentry-trace"),
+                    HeaderName::from_static("baggage"),
+                ])
+                .allow_methods([Method::POST, Method::GET, Method::PUT, Method::OPTIONS])
+                .allow_origin(AllowOrigin::predicate(|origin, _| {
+                    if let Ok(host) = origin.to_str() {
+                        is_host_or_origin_from_preview_domain(host) || host == "yral.com"
+                    } else {
+                        false
+                    }
+                })),
+        )
+        .leptos_routes_with_handler(routes, get(leptos_routes_handler))
+        .fallback(file_and_error_handler)
+        // .layer(trace_layer)
+        .layer(sentry_tower_layer)
+        .with_state(res.app_state);
+
+    #[cfg(feature = "enable-oltp")]
+    let app = app.layer(trace_layer);
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    println!("listening on http://{}", &addr);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<std::net::SocketAddr>(),
+    )
+    .with_graceful_shutdown(terminate)
+    .await
+    .unwrap();
+
+    // Cleanup telemetry providers if they were initialized
+    #[cfg(feature = "enable-oltp")]
+    if let Some((logger_provider, tracer_provider, metrics_provider)) = telemetry_handles {
+        if let Some(logger_provider) = logger_provider {
+            if let Err(e) = logger_provider.shutdown() {
+                eprintln!("Error shutting down logger provider: {e}");
+            }
+        }
+        if let Err(e) = tracer_provider.shutdown() {
+            eprintln!("Error shutting down tracer provider: {e}");
+        }
+        if let Some(metrics_provider) = metrics_provider {
+            if let Err(e) = metrics_provider.shutdown() {
+                eprintln!("Error shutting down metrics provider: {e}");
+            }
+        }
+        tracing::info!("Telemetry providers shut down");
+    }
+    Ok(())
+}
+
+#[cfg(feature = "enable-oltp")]
+fn setup_telemetry() -> Option<(
+    Option<opentelemetry_sdk::logs::LoggerProvider>,
+    opentelemetry_sdk::trace::TracerProvider,
+    Option<opentelemetry_sdk::metrics::SdkMeterProvider>,
+)> {
     let telemetry_handles = if let Ok(otlp_endpoint) = std::env::var("OTLP_ENDPOINT") {
         // Use OtlpTracesOnly for Jaeger - traces include latency metrics
         // Logs emitted during request handling will appear as span events in Jaeger
@@ -180,155 +344,7 @@ async fn main_impl() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     };
-
-    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
-    // For deployment these variables are:
-    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
-    // Alternately a file can be specified such as Some("Cargo.toml")
-    // The file would need to be included with the executable when moved to deployment
-    let conf = get_configuration(None).unwrap();
-    let leptos_options = conf.leptos_options;
-    let addr = leptos_options.site_addr;
-    let routes = generate_route_list(App);
-
-    let res = AppStateBuilder::new(leptos_options, routes.clone())
-        .build()
-        .await;
-    let terminate = {
-        use tokio::signal;
-
-        let ctrl_c = async {
-            signal::ctrl_c()
-                .await
-                .expect("failed to install Ctrl+C handler");
-        };
-
-        #[cfg(unix)]
-        let terminate = async {
-            use tokio::signal;
-            signal::unix::signal(signal::unix::SignalKind::terminate())
-                .expect("failed to install signal handler")
-                .recv()
-                .await;
-        };
-
-        #[cfg(not(unix))]
-        let terminate = std::future::pending::<()>();
-
-        async {
-            tokio::select! {
-                _ = ctrl_c => {},
-                _ = terminate => {},
-            }
-            log::info!("stopping...");
-
-            #[cfg(feature = "local-bin")]
-            std::mem::drop(res.containers);
-        }
-    };
-
-    // Create HTTP tracing layer with OpenTelemetry semantic conventions
-    let trace_layer = tower_http::trace::TraceLayer::new_for_http()
-        .make_span_with(|request: &axum::extract::Request<_>| {
-            let method = request.method();
-            let uri = request.uri();
-            let route = request
-                .extensions()
-                .get::<axum::extract::MatchedPath>()
-                .map(|path| path.as_str())
-                .unwrap_or_else(|| uri.path());
-
-            tracing::info_span!(
-                "http_request",
-                method = %method,
-                uri = %uri,
-                route = route,
-                // OpenTelemetry semantic conventions
-                otel.name = format!("{} {}", method, route),
-                otel.kind = "server",
-                http.method = %method,
-                http.url = %uri,
-                http.route = route,
-                service.name = "yral_ssr",
-            )
-        })
-        .on_response(
-            |response: &axum::response::Response,
-             latency: std::time::Duration,
-             _span: &tracing::Span| {
-                tracing::info!(
-                    status_code = response.status().as_u16(),
-                    latency_ms = latency.as_millis(),
-                    "request completed"
-                );
-            },
-        );
-
-    let sentry_tower_layer = ServiceBuilder::new()
-        .layer(NewSentryLayer::new_from_top())
-        .layer(SentryHttpLayer::with_transaction());
-
-    // build our application with a route
-    let app = Router::new()
-        .route(
-            "/api/{*fn_name}",
-            get(server_fn_handler).post(server_fn_handler),
-        )
-        .layer(
-            CorsLayer::new()
-                .allow_credentials(true)
-                .allow_headers([
-                    header::AUTHORIZATION,
-                    header::CONTENT_TYPE,
-                    header::ACCEPT,
-                    HeaderName::from_static("sentry-trace"),
-                    HeaderName::from_static("baggage"),
-                ])
-                .allow_methods([Method::POST, Method::GET, Method::PUT, Method::OPTIONS])
-                .allow_origin(AllowOrigin::predicate(|origin, _| {
-                    if let Ok(host) = origin.to_str() {
-                        is_host_or_origin_from_preview_domain(host) || host == "yral.com"
-                    } else {
-                        false
-                    }
-                })),
-        )
-        .leptos_routes_with_handler(routes, get(leptos_routes_handler))
-        .fallback(file_and_error_handler)
-        .layer(trace_layer)
-        .layer(sentry_tower_layer)
-        .with_state(res.app_state);
-
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    println!("listening on http://{}", &addr);
-    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    axum::serve(
-        listener,
-        app.into_make_service_with_connect_info::<std::net::SocketAddr>(),
-    )
-    .with_graceful_shutdown(terminate)
-    .await
-    .unwrap();
-
-    // Cleanup telemetry providers if they were initialized
-    if let Some((logger_provider, tracer_provider, metrics_provider)) = telemetry_handles {
-        if let Some(logger_provider) = logger_provider {
-            if let Err(e) = logger_provider.shutdown() {
-                eprintln!("Error shutting down logger provider: {e}");
-            }
-        }
-        if let Err(e) = tracer_provider.shutdown() {
-            eprintln!("Error shutting down tracer provider: {e}");
-        }
-        if let Some(metrics_provider) = metrics_provider {
-            if let Err(e) = metrics_provider.shutdown() {
-                eprintln!("Error shutting down metrics provider: {e}");
-            }
-        }
-        tracing::info!("Telemetry providers shut down");
-    }
-    Ok(())
+    telemetry_handles
 }
 
 fn main() {
